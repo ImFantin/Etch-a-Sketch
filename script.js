720f7379e58e5d3c2848b11426499a0a558a8ef0
Fantinn <dougfantin@icloud.com>
2023-12-15 11:12:43 -0800

    User can update grid using the size slider.

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-let date = new Date();
-let year = date.getFullYear();
-let footerP = document.getElementById('updateYear');
-updateYear.innerHTML = `Copyright &copy ${year} Douglas Fantin`;
-
-let container = document.getElementById('gridContainer');
-
-function clear() {
-  container.innerHTML = '';
-}
-
 function createGrid(rows, columns) {
-  clear();
-  
+  let container = document.getElementById('gridContainer');
+  container.innerHTML = '';
+
   let cellSize = `${100 / columns}%`;
   
   for (let i = 0; i < rows * columns; i++) {
     let cell = document.createElement('div');
+    
     cell.classList.add('grid-cell');
     cell.style.width = cellSize;
     container.appendChild(cell);
   }
 }
 
+function updateGridSize(value, size) {
+  let valueOutput = document.getElementById(value);
+  let gridSize = document.getElementById(size);
+
+  valueOutput.innerHTML = `${gridSize.value} x ${gridSize.value}`;
+
+  createGrid(gridSize.value, gridSize.value);
+}
+
+function updateDate() {
+  let date = new Date();
+  let year = date.getFullYear();
+  let footerP = document.getElementById('updateYear');
+  footerP.innerHTML = `Copyright &copy ${year} Douglas Fantin`;
+}
+
 window.addEventListener('load', () => {
-  createGrid(16, 16);
+  updateDate();
+  createGrid(8, 8);
+
+  let value = 'sizeValue';
+  let size = 'sizeSlider';
+
+  document.getElementById(size).addEventListener('input', (event) => {
+    updateGridSize(value, size);
+  });
 });
