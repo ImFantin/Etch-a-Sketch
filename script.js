8044884cb5d98bc03fd3f85cec530795daba9819
Fantinn <dougfantin@icloud.com>
2023-12-16 12:21:13 -0800

    Add painting, erase, clear functions.

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
+let defaultColor = '#333333';
+let eraseColor = '#EDEDED';
+
+let paintingMode;
+let colorPicker = document.getElementById('colorPicker');
+
+function addClickListener(id, handler) {
+  document.getElementById(id).addEventListener('click', handler);
+}
+
+function setPaintingMode(modeFunction) {
+  paintingMode = modeFunction;
+}
+
+function getDefaultColor() {
+  return colorPicker.value;
+}
+
+function getEraseColor() {
+  return eraseColor;
+}
+
+function rainbowMode() {
+  return getRandomColor();
+}
+
+function getRandomColor() {
+  return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+}
+
 function createGrid(rows, columns) {
   let container = document.getElementById('gridContainer');
   container.innerHTML = '';
 
   let cellSize = `${100 / columns}%`;
-  
+
   for (let i = 0; i < rows * columns; i++) {
     let cell = document.createElement('div');
-    
+
     cell.classList.add('grid-cell');
     cell.style.width = cellSize;
     container.appendChild(cell);
   }
+
+  container.addEventListener('mousedown', startPainting);
+  container.addEventListener('mousemove', paintGrid);
+  container.addEventListener('mouseup', stopPainting);
 }
 
 function updateGridSize(value, size) {
@@ -20,6 +54,38 @@ function updateGridSize(value, size) {
   valueOutput.innerHTML = `${gridSize.value} x ${gridSize.value}`;
 
   createGrid(gridSize.value, gridSize.value);
+}
+
+let lastPaintedCell;
+
+function paintGrid(e) {
+  if (!isPainting) return;
+
+  let chosenColor;
+  let currentCell = e.target;
+
+  if (currentCell !== lastPaintedCell) {
+    if (paintingMode === rainbowMode) {
+      chosenColor = getRandomColor();
+    } else {
+      chosenColor = paintingMode();
+    }
+
+    currentCell.style.backgroundColor = chosenColor;
+
+    lastPaintedCell = currentCell;
+  }
+}
+
+let isPainting = false;
+
+function startPainting(e) {
+  isPainting = true;
+  paintGrid(e);
+}
+
+function stopPainting() {
+  isPainting = false;
 }
 
 function updateDate() {
@@ -35,6 +101,16 @@ window.addEventListener('load', () => {
 
   let value = 'sizeValue';
   let size = 'sizeSlider';
+
+  setPaintingMode(getDefaultColor);
+
+  addClickListener('default', () => setPaintingMode(getDefaultColor));
+  addClickListener('random', () => setPaintingMode(rainbowMode));
+  addClickListener('erase', () => setPaintingMode(getEraseColor));
+  addClickListener('clear', () => {
+    let gridSize = document.getElementById('sizeSlider');
+    createGrid(gridSize.value, gridSize.value);
+  });
 
   document.getElementById(size).addEventListener('input', (event) => {
     updateGridSize(value, size);
